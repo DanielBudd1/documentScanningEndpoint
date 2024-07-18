0691a24abd6a835518acd47a3ff3a8f3e5512ccb
DanielBudd1 <daniel@convedo.com>
2024-07-18 13:52:57 +0100

    root

--- app.py
+++ app.py
@@ -15,6 +15,10 @@ app.add_middleware(
  allow_headers=["*"],
 )
 
+@app.get("/")
+async def root():
+    return {"message": "Hello World"}
+
 @app.get("/api/test/")
 async def test():
  return "Hello World!"
